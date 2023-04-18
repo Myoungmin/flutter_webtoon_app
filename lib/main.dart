ceb834d1fbc3c271fc36fc3dba84bb56b0b46400
AzureAD\고명민 <miung222@gmail.com>
2023-04-19 08:08:41 +0900

    AppBar 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webtoon_app/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -10,9 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(),
-      ),
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Todays's 툰s"),
+        centerTitle: true,
+      ),
+    );
   }
 }
