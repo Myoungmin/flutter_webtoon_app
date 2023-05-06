f78c8ca29c59681d02ded040d3c051bd29710fa1
AzureAD\고명민 <miung222@gmail.com>
2023-05-06 12:51:03 +0900

    ApiService를 생성하고 동작 확인

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev/";
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
   void getTodaysToons() async {
