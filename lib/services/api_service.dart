dc488ead3cda53c8db948be17d03d06277666fa0
AzureAD\고명민 <miung222@gmail.com>
2023-05-15 21:01:15 +0900

    async 함수 Future 사용

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,19 +4,19 @@ import 'package:flutter_webtoon_app/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  List<WebtoonModel> webtoonInstances = [];
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        final toon = WebtoonModel.fromJson(webtoon);
-        print(toon.title);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-      return;
+      return webtoonInstances;
     }
     throw Error();
   }
