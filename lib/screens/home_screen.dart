4c27b229a6e582c34b685af7992f77d1b248f05d
AzureAD\고명민 <miung222@gmail.com>
2023-05-18 21:12:38 +0900

    initState에서 waitForWebToons으로 async 호출을 하고 print

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webtoon_app/models/webtoon_model.dart';
 
+import '../services/api_service.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -12,8 +14,22 @@ class _HomeScreenState extends State<HomeScreen> {
   List<WebtoonModel> webtoons = [];
   bool isLoading = true;
 
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
